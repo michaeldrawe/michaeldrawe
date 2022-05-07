73e1fd0040c64bbc1aac10932cc4915272d4e5c3
Michael Drawe <michaeldrawe@gmail.com>
2022-05-07 10:23:25 -0600

    fixed alignment for images and text on main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,31 +8,44 @@ const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
     <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
-      <div class="col-12 ">
-        
+      <div className="col-lg-1"></div>
+      <div class="col-lg-5 col-md-12 col-sm-12">
         <StaticImage
-            src="../images/gatsby-astronaut.png"
-            layout="fixed"
-            height={600}
-            quality={100}
-            formats={["AUTO", "WEBP"]}
-            alt="A Gatsby astronaut"
-            className="d-block mx-lg-auto img-fluid" />
+          src="../images/gatsby-astronaut.png"
+          layout="fixed"
+          height={600}
+          quality={100}
+          formats={["AUTO", "WEBP"]}
+          alt="A Gatsby astronaut"
+          className="d-block mx-lg-auto img-fluid"
+        />
       </div>
-      <div className="col-lg-12 text-center">
+      <div className="col-lg-5 col-md-12 col-sm-12 text-center">
         <h1 className="display-2 fw-bold lh-1 mb-3">Hello and welcome!</h1>
-        <h1 className="lead display-4 fw-bold lh-1 mb-3">I'm Michael Drawe</h1><hr/>
-        <p className=" display-5">Front-End Web Developer,</p>
-        <p className="display-5"> Designer and </p>
-        <p className="display-5"> always curious human</p>
-        <div class="d-grid gap-4 d-xl-flex justify-content-xl-start">
-          <button type="button" className="btn btn-primary btn-lg px-4 me-md-2 justify-content-lg-middle">Check out my portfolio</button>
-          <button type="button" className="btn btn-outline-secondary btn-lg px-4">Contact Me</button>
-        </div>
+        <h1 className="lead display-4 fw-bold lh-1 mb-3">I'm Michael Drawe</h1>
+        <hr />
+        <p className=" display-5">Front-End Web Developer</p>
+        <p className="display-5">and </p>
+        <p className="display-5"> Always Curious Human</p>
       </div>
+      <div className="col-lg-1"></div>
     </div>
-    
-    
+    <div className="row">
+      <div class="d-grid gap-4 d-xl-flex justify-content-xl-center">
+        <div className="col-xl-3">
+          {" "}
+          <button
+            type="button"
+            className="btn btn-outline-secondary btn-lg px-4"
+          >
+            Contact Me
+          </button>{" "}
+          <button type="button" className="btn btn-primary btn-lg px-4 me-md-2">
+            Check out my portfolio
+          </button>
+        </div>
+      </div>{" "}
+    </div>
   </Layout>
 )
 
