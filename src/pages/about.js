8c068ef50968f4704fb80d1f88fb7a0b3f099a83
Michael Drawe <michaeldrawe@gmail.com>
2022-05-17 19:16:39 -0600

    portfolio project styled final version

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,17 +1,74 @@
 import * as React from 'react'
-
-
 import Layout from '../components/layout'
 import Seo from '../components/seo'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const AboutPage = () => (
   <Layout>
     <Seo title='About Page' />
     <div className='container my-3'>
       <h1 className='display-3 text-muted'>About Me</h1>
-      <p>I enjoy finding out what I don't know.</p>
-      <p>Now the important stuff...</p>
-      <div clsasName="row">col-md-12</div>
+
+      <div className='row'>
+        <div className='col-md-4 p-5 mt-5 mb-5 lh-1 border text-muted'>
+          <p>I enjoy finding out what I don't know.</p>
+          <p>
+            I enjoy the journey, even though sometimes I admit, I don't know the
+            ending.
+          </p>
+        </div>
+        <div className='col-md-8 mt-5 text-white'>
+          <StaticImage
+            src='../images/eye-4453129.jpg'
+            layout='fixed'
+            height={400}
+            quality={100}
+            formats={['AUTO', 'WEBP']}
+            alt='Photo of Michael Drawe'
+            className='d-block mx-lg-auto img-fluid'
+          />
+          <small>
+            Image by{' '}
+            <a href='https://pixabay.com/users/nika_akin-13521770/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=4453129'>
+              Nika Akin
+            </a>{' '}
+            from{' '}
+            <a href='https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=4453129'>
+              Pixabay
+            </a>
+          </small>
+        </div>
+
+        <div className='col-md-8 pt-5 mb-5 lh-1 text-muted'>
+          <StaticImage
+            src='../images/eye-4453129.jpg'
+            layout='fixed'
+            height={400}
+            quality={100}
+            formats={['AUTO', 'WEBP']}
+            alt='Photo of Michael Drawe'
+            className='d-block mx-lg-auto img-fluid'
+          />
+          <small>
+            Image by{' '}
+            <a href='https://pixabay.com/users/nika_akin-13521770/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=4453129'>
+              Nika Akin
+            </a>{' '}
+            from{' '}
+            <a href='https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=4453129'>
+              Pixabay
+            </a>
+          </small>
+        </div>
+        <div className='col-md-6 pt-5 mb-5 text-muted'>
+          <p>
+            I enjoy finding out what I don't know. I enjoy the journey, even
+            though sometimes I admit, I don't know the ending.
+          </p>
+          <p>Now the important stuff...</p>
+        </div>
+        <div className='col-md-6 pt-5 mb-5 text-muted'></div>
+      </div>
     </div>
   </Layout>
 )
--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -30,81 +30,36 @@ const PortfolioPage = () => (
             </div>
             <div class='card-footer'>
               <div className='row text-muted'>
-                <div className='col'>Code</div>
-                <div className='col'>App</div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className='col-sm-12 col-md-6 col-lg-4 col-xl-3 pt-3'>
-          <div className='card card-cover h-100 overflow-hidden text-white bg-dark rounded-4 shadow-lg'>
-            <StaticImage
-              src='../images/michael steph and kurtis.png'
-              width={300}
-              height={300}
-              quality={100}
-              formats={['AUTO', 'WEBP']}
-              alt='A Gatsby astronaut'
-              className='card-img'
-            />
-            <div class='card-body'>
-              <h5 class='card-title display-6'>Card title</h5>
-              <p class='card-text'>
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </p>
-            </div>
-            <div class='card-footer'>
-              <div className='row text-muted'>
-                <div className='col'>
-                  <a href='#'>
-                    <svg
-                      xmlns='http://www.w3.org/2000/svg'
-                      width='32'
-                      height='32'
-                      fill='#4fa2f5'
-                      viewBox='0 0 64 64'
-                    >
-                      <path d='M414.8 40.79L286.8 488.8C281.9 505.8 264.2 515.6 247.2 510.8C230.2 505.9 220.4 488.2 225.2 471.2L353.2 23.21C358.1 6.216 375.8-3.624 392.8 1.232C409.8 6.087 419.6 23.8 414.8 40.79H414.8zM518.6 121.4L630.6 233.4C643.1 245.9 643.1 266.1 630.6 278.6L518.6 390.6C506.1 403.1 485.9 403.1 473.4 390.6C460.9 378.1 460.9 357.9 473.4 345.4L562.7 256L473.4 166.6C460.9 154.1 460.9 133.9 473.4 121.4C485.9 108.9 506.1 108.9 518.6 121.4V121.4zM166.6 166.6L77.25 256L166.6 345.4C179.1 357.9 179.1 378.1 166.6 390.6C154.1 403.1 133.9 403.1 121.4 390.6L9.372 278.6C-3.124 266.1-3.124 245.9 9.372 233.4L121.4 121.4C133.9 108.9 154.1 108.9 166.6 121.4C179.1 133.9 179.1 154.1 166.6 166.6V166.6z' />
-                    </svg>
+              <div className='col'>
+                <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    width='20'
+                    height='20'
+                    fill='#4fa2f5'
+                    class='bi bi-github'
+                    viewBox='0 0 16 16'
+                  >
+                    <path d='M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z' />
+                  </svg>
+                  <a href="#">{" "}Code</a>
+                  <a href="#">{" "}
                   </a>
-                  Code
                 </div>
-                <div className='col'>App</div>
+                <div className='col'>
+                <a href="#">{" "}
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-play-fill" viewBox="0 0 16 16"> 
+                <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z"/>
+</svg>{" "}App
+                  </a>
+                  </div>
               </div>
             </div>
           </div>
         </div>
 
-        <div className='col-sm-12 col-md-6 col-lg-4 col-xl-3 pt-3'>
-          <div className='card card-cover h-100 overflow-hidden text-white bg-dark rounded-4 shadow-lg'>
-            <StaticImage
-              src='../images/fuu-j-r2nJPbEYuSQ-unsplash.jpg'
-              width={300}
-              height={300}
-              quality={100}
-              formats={['AUTO', 'WEBP']}
-              alt='A Gatsby astronaut'
-              className='card-img'
-            />
-            <div class='card-body'>
-              <h5 class='card-title display-6'>Card title</h5>
-              <p class='card-text'>
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </p>
-            </div>
-            <div class='card-footer'>
-              <div className='row text-muted'>
-                <div className='col'>Code</div>
-                <div className='col'>App</div>
-              </div>
-            </div>
-          </div>
-        </div>
+        
+
+        
       </div>
     </div>
   </Layout>
